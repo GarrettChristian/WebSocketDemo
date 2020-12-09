111657c0caab01964ec27e5b0add0f5e75ddcef8
GarrettChristian <garrettchristian2@gmail.com>
2020-12-09 12:28:12 -0500

    adding files

---
+++ src/main/java/com/example/WebSocketDemo/HelloMessage.java
@@ -0,0 +1,22 @@
+package com.example.WebSocketDemo;
+
+public class HelloMessage {
+
+    private String name;
+
+    public HelloMessage() {
+    }
+
+    public HelloMessage(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public HelloMessage setName(String name) {
+        this.name = name;
+        return this;
+    }
+}
